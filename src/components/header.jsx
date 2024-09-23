9dbea2a9c76c6561ee87e3bd29e8d3ccac53cd4c
Vicky Dutta <duttakoushik779@gmail.com>
2024-09-23 12:12:58 +0530

    chore: change the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="w-full text-white rounded-tr-full rounded-l-full flex flex-col bg-purple-700  text-start py-3 pl-6 mb-5 shadow-xl shadow-blue-100">
       <h1 className="w-fit text-2xl md:text-3xl bg-white text-purple-700 pr-3 rounded-lg">
-        MERN CHURIAN
+        MERN CHATAPP
       </h1>
       <span className="md:text-xs pl-5 text-[10px]">
         Where Conversations Meet Technology!
